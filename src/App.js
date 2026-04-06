8ec339a329e09e8c87ab1da8a4976f093c5dcd03
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-04-06 23:47:11 +0530

    k

--- src/App.js
+++ src/App.js
@@ -2,34 +2,54 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [firstName, setFirstName]=useState('');
-  const [lastName, setLastName]=useState('')
+  // const [firstName, setFirstName]=useState('');
+  // const [lastName, setLastName]=useState('')
 
-  console.log(firstName);
-  console.log(lastName);
+  // console.log(firstName);
+  // console.log(lastName);
   
   
-  function changefirstNameHandler(event) {
-    // console.log("Printing first name");
-    // console.log(event.target.value);
-    setFirstName(event.target.value);
+  // function changefirstNameHandler(event) {
+  //   // console.log("Printing first name");
+  //   // console.log(event.target.value);
+  //   setFirstName(event.target.value);
     
+  // }
+  // function changelastNameHandler (event) {
+  //   // console.log("Printing last name");
+  //   // console.log(event.target.value);
+  //   setLastName(event.target.value)
+  
+  const [formData, setFormData]=useState({firstName:"" , lastName:"", email:""});
+  
+  function changeHandler(event) {
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [event.target.name]:event.target.value
+      }
+    });
   }
-  function changelastNameHandler (event) {
-    // console.log("Printing last name");
-    // console.log(event.target.value);
-    setLastName(event.target.value)
-  }
+
+
   return (
     <div className='App'>
       <form>
         <input type='text'
         placeholder='first-name'
-        onChange={changefirstNameHandler}/>
+        onChange={changeHandler}
+        name='first-Name'/>
         <br/><br/>
         <input type='text'
         placeholder='last-name'
-        onChange={changelastNameHandler}/>
+        onChange={changeHandler}
+        name='last-Name'/>
+
+        <br/><br/>
+        <input type='email'
+        placeholder='enter your email here'
+        onChange={changeHandler}
+        name='email'/>
 
 
 
@@ -37,6 +57,6 @@ function App() {
       </form>
     </div>
   );
-}
 
+}
 export default App;
